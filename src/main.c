5542f5cf25e7628422b3a2a9c8d8fb632926d628
Guillaume Chainet <gchainet@gmail.com>
2018-12-22 12:31:16 +0100

    Refactor main function

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: gchainet <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/12 07:14:15 by gchainet          #+#    #+#             */
-/*   Updated: 2018/12/22 12:13:01 by gchainet         ###   ########.fr       */
+/*   Updated: 2018/12/22 12:30:58 by gchainet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,9 +17,19 @@
 #include "libft.h"
 #include "fill_line.h"
 
+static void	exec_ast(t_shell *shell, t_token *tokens)
+{
+	t_ast	*ast;
+
+	if ((ast = parse(shell, tokens)))
+	{
+		ast->exec(shell, ast);
+		ast->del(ast);
+	}
+}
+
 int			main(int ac, char **av, char **environ)
 {
-	t_ast		*ast;
 	t_shell		shell;
 	t_token		*tokens;
 
@@ -35,11 +45,7 @@ int			main(int ac, char **av, char **environ)
 			ft_printf(">");
 		else
 		{
-			if ((ast = parse(&shell, tokens)))
-			{
-				ast->exec(&shell, ast);
-				ast->del(ast);
-			}
+			exec_ast(&shell, tokens);
 			ft_printf("$> ");
 		}
 		free(shell.line);
