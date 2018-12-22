5d15fa78ea6c690f7770dc21f5223109f3695f0d
Charles Vignal <cvignal@student.42.fr>
2018-12-22 13:51:52 +0100

    small fix for the history before merge

--- src/parser/history.c
+++ src/parser/history.c
@@ -6,7 +6,7 @@
 /*   By: cvignal <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/21 10:29:40 by cvignal           #+#    #+#             */
-/*   Updated: 2018/12/21 18:15:21 by cvignal          ###   ########.fr       */
+/*   Updated: 2018/12/22 13:50:54 by cvignal          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,10 +38,12 @@ void	ft_hisdown(t_cmdline *res, t_shell *shell)
 	}
 	clear_cmd_line(res);
 	if (res->his_pos > -1)
+	{
 		ft_printf("%s", curr->content);
-	res->cursor = curr->content_size;
-	ft_strdel(&res->str);
-	res->str = ft_strdup(curr->content);
+		res->cursor = curr->content_size;
+		ft_strdel(&res->str);
+		res->str = ft_strdup(curr->content);
+	}
 }
 
 void	ft_hisup(t_cmdline *res, t_shell *shell)
@@ -53,7 +55,7 @@ void	ft_hisup(t_cmdline *res, t_shell *shell)
 	if (!(curr = shell->history))
 		return ;
 	i = 0;
-	if (res->his_pos < (int)ft_lstlen(shell->history))
+	if (res->his_pos < (int)ft_lstlen(shell->history) - 1)
 		res->his_pos++;
 	while (i < res->his_pos && curr->next)
 	{
