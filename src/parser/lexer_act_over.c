49eb147c0ae7a5adc4aa0536baf28edaea2717a4
Guillaume Chainet <gchainet@gmail.com>
2018-12-22 11:58:32 +0100

    Add \n to token when doing multiple lines quotes

--- src/parser/lexer_act_over.c
+++ src/parser/lexer_act_over.c
@@ -6,7 +6,7 @@
 /*   By: gchainet <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/22 10:08:55 by gchainet          #+#    #+#             */
-/*   Updated: 2018/12/22 11:28:37 by gchainet         ###   ########.fr       */
+/*   Updated: 2018/12/22 11:57:41 by gchainet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,7 +24,8 @@ int	lexer_over(struct s_shell *shell, t_token *token, char c)
 int	lexer_more_input(t_shell *shell, t_token *token, char c)
 {
 	(void)shell;
-	(void)token;
 	(void)c;
+	if (add_to_token(token, '\n'))
+		return (1 << LEXER_RET_ERROR);
 	return (1 << LEXER_RET_MORE_INPUT);
 }
