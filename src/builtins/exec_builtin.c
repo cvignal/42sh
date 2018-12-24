2a4106fd51bcdb51cf6bb6676bae89f4dc28427d
Guillaume Chainet <gchainet@gmail.com>
2018-12-24 12:09:18 +0100

    Fix redirection from builtin by saving std fd before applying redirections

--- src/builtins/exec_builtin.c
+++ src/builtins/exec_builtin.c
@@ -6,7 +6,7 @@
 /*   By: gchainet <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/21 11:21:38 by gchainet          #+#    #+#             */
-/*   Updated: 2018/12/23 18:52:34 by gchainet         ###   ########.fr       */
+/*   Updated: 2018/12/24 12:08:24 by gchainet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,9 +17,6 @@
 
 static int	pipe_builtin(t_pipeline *current)
 {
-	current->fd_copy[STDIN_FILENO] = dup(STDIN_FILENO);
-	current->fd_copy[STDOUT_FILENO] = dup(STDOUT_FILENO);
-	current->fd_copy[STDERR_FILENO] = dup(STDERR_FILENO);
 	if (current->in_fd[STDIN_FILENO] != -1)
 	{
 		dup2(current->in_fd[STDIN_FILENO], STDIN_FILENO);
@@ -30,6 +27,14 @@ static int	pipe_builtin(t_pipeline *current)
 		dup2(current->out_fd[STDOUT_FILENO], STDOUT_FILENO);
 		close(current->out_fd[STDOUT_FILENO]);
 	}
+	return (0);
+}
+
+static int	save_fd(t_pipeline *current)
+{
+	current->fd_copy[STDIN_FILENO] = dup(STDIN_FILENO);
+	current->fd_copy[STDOUT_FILENO] = dup(STDOUT_FILENO);
+	current->fd_copy[STDERR_FILENO] = dup(STDERR_FILENO);
 	return (0);
 }
 
@@ -48,6 +53,7 @@ int	exec_builtin(t_shell *shell, t_builtin builtin, t_pipeline *current)
 {
 	int	ret;
 
+	save_fd(current);
 	apply_redirs(shell, current->command);
 	pipe_builtin(current);
 	ret = builtin(shell, current->command->args);
