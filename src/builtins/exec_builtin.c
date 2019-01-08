38a36faff735ea634cd4a8b15ba29afd21923bf1
Guillaume Chainet <gchainet@gmail.com>
2019-01-08 07:45:57 +0100

    Fix pipes for builtins

--- src/builtins/exec_builtin.c
+++ src/builtins/exec_builtin.c
@@ -6,7 +6,7 @@
 /*   By: gchainet <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/21 11:21:38 by gchainet          #+#    #+#             */
-/*   Updated: 2019/01/08 07:28:55 by gchainet         ###   ########.fr       */
+/*   Updated: 2019/01/08 07:45:11 by gchainet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,8 +15,10 @@
 
 #include "21sh.h"
 
-static int	prepare_pipeline(t_ast *instr)
+static int	prepare_pipeline(t_ast *instr, int *old)
 {
+	old[STDIN_FILENO] = dup(STDIN_FILENO);
+	old[STDOUT_FILENO] = dup(STDOUT_FILENO);
 	if (instr->pipes_in[PIPE_PARENT][STDIN_FILENO] != -1)
 	{
 		dup2(instr->pipes_in[PIPE_PARENT][STDIN_FILENO], STDIN_FILENO);
@@ -32,8 +34,14 @@ static int	prepare_pipeline(t_ast *instr)
 
 int			exec_builtin(t_shell *shell, t_builtin builtin, t_ast *instr)
 {
-	prepare_pipeline(instr);
+	int		fd[2];
+
+	prepare_pipeline(instr, fd);
 	apply_redirs(shell, instr);
 	instr->ret = builtin(shell, ((t_command *)instr->data)->args);
+	dup2(fd[STDIN_FILENO], STDIN_FILENO);
+	dup2(fd[STDOUT_FILENO], STDOUT_FILENO);
+	close(fd[STDIN_FILENO]);
+	close(fd[STDOUT_FILENO]);
 	return (instr->ret);
 }
