d02a0db7b06f2d28cb371dc5654b0f3a25519b47
Charles VIGNAL <cvignal@student.42.fr>
2019-01-25 14:13:43 +0100

    Cursor motion for multilines historic improved

--- src/parser/tools_motion.c
+++ src/parser/tools_motion.c
@@ -6,15 +6,18 @@
 /*   By: cvignal <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/24 11:28:55 by cvignal           #+#    #+#             */
-/*   Updated: 2019/01/24 18:12:58 by cvignal          ###   ########.fr       */
+/*   Updated: 2019/01/25 11:26:57 by cvignal          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include <term.h>
+#include <curses.h>
 
 #include "libft.h"
 #include "fill_line.h"
 #include "shell.h"
 
-size_t	length_line(t_shell *shell)
+size_t	length_prev_line(t_shell *shell)
 {
 	size_t	ret;
 	size_t	j;
@@ -36,4 +39,40 @@ size_t	length_line(t_shell *shell)
 	if (!k)
 		ret += 4;
 	return (ret);
+}
+
+size_t	length_curr_line(t_shell *shell)
+{
+	size_t	ret;
+	size_t	j;
+
+	j = shell->line.cursor + 1;
+	ret = 0;
+	if (shell->line.data[j] == '\n')
+		return (0);
+	while (j < shell->line.len)
+	{
+		j++;
+		ret++;
+		if (shell->line.data[j] == '\n')
+			break ;
+	}
+	return (ret);
+}
+
+void	scroll_lines(t_shell *shell)
+{
+	size_t	len;
+
+	if (shell->line.data[shell->line.cursor] == '\n')
+	{
+		len = length_curr_line(shell);
+		tputs(tgetstr("sc", NULL), 0, ft_printchar);
+		if (len)
+			ft_printf("%.*s", len + 1, shell->line.data
+					+ shell->line.cursor + 1);
+		tputs(tgetstr("do", NULL), 0, ft_printchar);
+		tputs(tgetstr("dl", NULL), 0, ft_printchar);
+		tputs(tgetstr("rc", NULL), 0, ft_printchar);
+	}
 }
