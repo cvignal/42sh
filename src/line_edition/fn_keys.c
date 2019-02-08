6ce13f7848e9fdf2c799bf8718691fedcd4c2cae
Charles VIGNAL <cvignal@student.42.fr>
2019-02-08 14:10:33 +0100

    Ctrl C in heredocs now works

--- src/line_edition/fn_keys.c
+++ src/line_edition/fn_keys.c
@@ -6,7 +6,7 @@
 /*   By: cvignal <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/22 16:24:09 by cvignal           #+#    #+#             */
-/*   Updated: 2019/02/07 15:51:17 by cvignal          ###   ########.fr       */
+/*   Updated: 2019/02/08 14:04:46 by cvignal          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -86,7 +86,7 @@ int	ft_ctrlc(t_shell *shell)
 		clean_exit_lexer(&shell->lexer, &shell->output, &shell->current, NULL);
 	free_line(&shell->line);
 	if (shell->ctrld)
-		shell->end_heredoc = 1;
+		shell->end_heredoc = 2;
 	else
 		ft_printf("\n$> ");
 	return (shell->end_heredoc);
--- src/signal.c
+++ src/signal.c
@@ -6,7 +6,7 @@
 /*   By: cvignal <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/15 13:50:27 by cvignal           #+#    #+#             */
-/*   Updated: 2019/01/31 11:13:51 by cvignal          ###   ########.fr       */
+/*   Updated: 2019/02/08 13:53:22 by cvignal          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,7 +24,6 @@ static void	print_newline(int i)
 void		disable_signal(void)
 {
 	signal(SIGINT, print_newline);
-	signal(SIGQUIT, print_newline);
 }
 
 void		enable_signal(void)
