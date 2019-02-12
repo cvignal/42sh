a911eb28c687b558744f60dab56d48b3344b35cc
Guillaume CHAINET <gchainet@sudent.42.fr>
2019-02-12 21:14:55 +0100

    Remove debug function in cmd.c

--- src/exec/cmd.c
+++ src/exec/cmd.c
@@ -6,7 +6,7 @@
 /*   By: gchainet <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/15 09:43:54 by gchainet          #+#    #+#             */
-/*   Updated: 2019/02/12 21:10:53 by gchainet         ###   ########.fr       */
+/*   Updated: 2019/02/12 21:14:43 by gchainet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,27 +14,6 @@
 
 #include "shell.h"
 #include "ast.h"
-
-static void	debug(t_command *command)
-{
-	unsigned int	len;
-	unsigned int	i;
-
-	i = 0;
-	len = 0;
-	while (command->args[len])
-		++len;
-	ft_printf("len args: %u\n", len);
-	while (i < len)
-		ft_printf("%s\n", command->args[i++]);
-	len = 0;
-	i = 0;
-	while (command->args_value[len])
-		++len;
-	ft_printf("len value: %u\n", len);
-	while (i < len)
-		ft_printf("%s\n", command->args_value[i++]);
-}
 
 int		exec_cmd(t_shell *shell, t_ast *ast)
 {
