0139014d75bf08d6bf1ed1240f7c79b805cbff42
Charles VIGNAL <cvignal@student.42.fr>
2019-02-15 19:03:54 +0100

    New flag for ctrlc in heredocs

--- inc/shell.h
+++ inc/shell.h
@@ -6,7 +6,7 @@
 /*   By: gchainet <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/10 09:56:58 by gchainet          #+#    #+#             */
-/*   Updated: 2019/02/15 17:59:37 by cvignal          ###   ########.fr       */
+/*   Updated: 2019/02/15 18:59:22 by cvignal          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -83,6 +83,7 @@ typedef struct		s_shell
 	int				fd_op;
 	t_fd			*used_fd;
 	int				ctrld;
+	int				ctrlc;
 	int				end_heredoc;
 	int				prev_cmd_state;
 	t_token			*output;
