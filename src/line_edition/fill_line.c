205d67022e74ec35f9ff07ca8c6ac0b732e8b3fc
Charles VIGNAL <cvignal@student.42.fr>
2019-02-16 12:22:19 +0100

    alternate fill_line updated
    Former-commit-id: 2a2212bf6401cf5a38120ad027bc4ccfbb1ff997

--- src/line_edition/fill_line.c
+++ src/line_edition/fill_line.c
@@ -6,7 +6,7 @@
 /*   By: cvignal <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/18 14:41:08 by cvignal           #+#    #+#             */
-/*   Updated: 2019/02/16 11:04:42 by cvignal          ###   ########.fr       */
+/*   Updated: 2019/02/16 11:58:26 by cvignal          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -77,12 +77,11 @@ int		alt_fill_line(t_shell *shell)
 {
 	char	*line;
 
-	ft_dprintf(2, NOT_A_TTY);
 	reset_terminal_mode(shell);
 	shell->fd_op = 1;
 	g_fd_output = 1;
 	shell->ctrlc = 0;
-	if (get_next_line(0, &line) <= 0)
+	if (get_next_line(STDIN_FILENO, &line) <= 0)
 		return (1);
 	shell->line.data = line;
 	shell->line.len = ft_strlen(line);
