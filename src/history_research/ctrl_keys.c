279755e074b23932b5f1bb99cb20384d16813bd5
Charles VIGNAL <cvignal@student.42.fr>
2019-02-20 14:57:49 +0100

    History search first version, working but not with all features
    Former-commit-id: f1a42c871ebdd1ec4be1cea4c73e8ba3b239d3c3

---
+++ src/history_research/ctrl_keys.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ctrl_keys.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cvignal <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/02/20 09:57:21 by cvignal           #+#    #+#             */
+/*   Updated: 2019/02/20 14:35:12 by cvignal          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "shell.h"
+#include "fill_line.h"
+
+int	hs_linedown(t_shell *shell)
+{
+	(void)shell;
+	return (1);
+}
+
+int	hs_lineup(t_shell *shell)
+{
+	(void)shell;
+	return (1);
+}
+
+int	hs_ctrld(t_shell *shell)
+{
+	(void)shell;
+	return (1);
+}
+
+int	hs_ctrlc(t_shell *shell)
+{
+	(void)shell;
+	return (1);
+}
