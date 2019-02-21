dbd57c8f86e7c0dbb9277771d8100a902d21926f
Charles VIGNAL <cvignal@student.42.fr>
2019-02-21 14:25:37 +0100

    Fixed segfaults when using non alpha num in variable names and when copy/pasting from outside the shell
    Former-commit-id: fcc0d1eedaf63e31b8da54cb2fc12fe31910d5b2

--- src/builtins/setenv.c
+++ src/builtins/setenv.c
@@ -6,7 +6,7 @@
 /*   By: gchainet <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/13 08:14:44 by gchainet          #+#    #+#             */
-/*   Updated: 2019/02/15 19:38:10 by gchainet         ###   ########.fr       */
+/*   Updated: 2019/02/21 14:22:57 by cvignal          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,6 +29,28 @@ static int	print_env(t_shell *shell)
 	return (0);
 }
 
+static int	valid_value(char *str)
+{
+	int	i;
+
+	if (!ft_isalpha(str[0]) && str[0] != '_')
+		return (1);
+	i = 1;
+	while (str[i])
+	{
+		if (!ft_isalnum(str[i]) && str[i] != '_')
+			return (1);
+		i++;
+	}
+	return (0);
+}
+
+static int	setenv_error_display(char *str)
+{
+	ft_dprintf(2, "%s: %s\n", EXEC_NAME, str);
+	return (1);
+}
+
 int			builtin_setenv(t_shell *shell, char **args)
 {
 	size_t	arg_count;
@@ -41,10 +63,9 @@ int			builtin_setenv(t_shell *shell, char **args)
 	else if (arg_count == 3)
 	{
 		if (ft_strlen(args[1]) > VAR_MAX || ft_strlen(args[2]) > VAR_MAX)
-		{
-			ft_dprintf(2, "%s: variable too long\n", EXEC_NAME);
-			return (1);
-		}
+			return (setenv_error_display(ERR_LEN_VAR));
+		else if (valid_value(args[1]) || valid_value(args[2]))
+			return (setenv_error_display(ERR_CHAR_VAR));
 		if (set_env_var(shell, args[1], args[2]))
 			return (1);
 		if (!ft_strcmp(args[1], "PATH"))
