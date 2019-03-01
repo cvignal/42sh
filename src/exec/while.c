99ff01d3fe60fd72c529895d6d93be7ed1c9cc79
Charles VIGNAL <cvignal@student.42.fr>
2019-03-01 20:41:11 +0100

    Ctrl c fixed in while script
    Former-commit-id: 7cee3d7a92a0afbec3c88e90f14e2c1aeca794dc

--- src/exec/while.c
+++ src/exec/while.c
@@ -6,15 +6,24 @@
 /*   By: gchainet <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/31 17:57:58 by gchainet          #+#    #+#             */
-/*   Updated: 2019/02/18 14:49:33 by gchainet         ###   ########.fr       */
+/*   Updated: 2019/03/01 20:39:52 by cvignal          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
+#include <signal.h>
 
 #include "shell.h"
 #include "parser.h"
 #include "ast.h"
+
+int		g_flag = 0;
+
+void	local_signal(int i)
+{
+	(void)i;
+	g_flag = 1;
+}
 
 int		exec_while(t_shell *shell, t_ast *ast)
 {
@@ -22,13 +31,16 @@ int		exec_while(t_shell *shell, t_ast *ast)
 		return (0);
 	((t_ast *)ast->data)->exec(shell, ast->data);
 	wait_loop(ast->data);
-	while (((t_ast *)ast->data)->ret == 0)
+	signal(SIGINT, local_signal);
+	while (((t_ast *)ast->data)->ret == 0 && !g_flag)
 	{
 		ast->left->exec(shell, ast->left);
 		wait_loop(ast->left);
 		((t_ast *)ast->data)->exec(shell, ast->data);
 		wait_loop(ast->data);
 	}
+	g_flag = 0;
+	disable_signal();
 	reset_redirs(shell, ast);
 	return (ast->left->ret);
 }
