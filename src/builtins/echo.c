6208b62d524d0abc007c3cea0048f53a99b972d6
Charles VIGNAL <cvignal@student.42.fr>
2019-03-01 15:30:02 +0100

    Better implementation for echo

--- src/builtins/echo.c
+++ src/builtins/echo.c
@@ -6,26 +6,42 @@
 /*   By: gchainet <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/13 07:36:11 by gchainet          #+#    #+#             */
-/*   Updated: 2019/02/11 11:34:17 by cvignal          ###   ########.fr       */
+/*   Updated: 2019/03/01 15:29:44 by cvignal          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "shell.h"
 #include "libft.h"
 
-int	builtin_echo(t_shell *shell, char **args)
+static int	options_echo(char **args)
+{
+	int	i;
+	int	j;
+
+	i = 1;
+	while (args[i] && args[i][0] == '-')
+	{
+		j = 1;
+		while (args[i][j])
+		{
+			if (args[i][j] != 'n')
+				return (i);
+			j++;
+		}
+		i++;
+	}
+	return (i);
+}
+
+int			builtin_echo(t_shell *shell, char **args)
 {
 	int	nl;
 	int	i;
 
 	nl = 1;
-	i = 1;
 	(void)shell;
-	if (args[i] && !ft_strcmp(args[i], "-n"))
-	{
+	if ((i = options_echo(args)) > 1)
 		nl = 0;
-		++i;
-	}
 	while (args[i])
 	{
 		ft_printf("%s", args[i]);
