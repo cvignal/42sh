9b8207bbe72c0b92d28308fa37583fc50d482b09
Guillaume Chainet <gchainet@student.42.fr>
2019-04-12 21:40:49 +0200

    Implement binary mask in expand function to choose which expansion is performed and expand only variables in heredocs

--- src/expansion/lexer_methods_quotes.c
+++ src/expansion/lexer_methods_quotes.c
@@ -6,41 +6,43 @@
 /*   By: gchainet <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/08 21:12:58 by gchainet          #+#    #+#             */
-/*   Updated: 2019/03/29 18:13:27 by gchainet         ###   ########.fr       */
+/*   Updated: 2019/04/12 21:36:02 by gchainet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "shell.h"
 #include "expand.h"
 
-int	exp_lexer_push_squote(struct s_shell *shell, t_exp_lexer *lexer, char c)
+int	exp_lexer_push_squote(struct s_shell *shell, char c, int mask)
 {
-	(void)shell;
 	(void)c;
-	if (add_to_exp_buff(&lexer->buffer, 0))
+	if (!(mask & EXP_LEXER_MASK_QUOTE))
+		return (exp_lexer_add_to_buff(shell, c, mask));
+	if (add_to_exp_buff(&shell->exp_lexer.buffer, 0))
 		return (EXP_LEXER_RET_ERROR);
-	--lexer->buffer.pos;
-	if (exp_ss_push(lexer, EXP_STATE_SQUOTE))
+	--shell->exp_lexer.buffer.pos;
+	if (exp_ss_push(&shell->exp_lexer, EXP_STATE_SQUOTE))
 		return (EXP_LEXER_RET_ERROR);
 	return (EXP_LEXER_RET_CONT);
 }
 
-int	exp_lexer_push_dquote(struct s_shell *shell, t_exp_lexer *lexer, char c)
+int	exp_lexer_push_dquote(struct s_shell *shell, char c, int mask)
 {
-	(void)shell;
 	(void)c;
-	if (add_to_exp_buff(&lexer->buffer, 0))
+	if (!(mask & EXP_LEXER_MASK_QUOTE))
+		return (exp_lexer_add_to_buff(shell, c, mask));
+	if (add_to_exp_buff(&shell->exp_lexer.buffer, 0))
 		return (EXP_LEXER_RET_ERROR);
-	--lexer->buffer.pos;
-	if (exp_ss_push(lexer, EXP_STATE_DQUOTE))
+	--shell->exp_lexer.buffer.pos;
+	if (exp_ss_push(&shell->exp_lexer, EXP_STATE_DQUOTE))
 		return (EXP_LEXER_RET_ERROR);
 	return (EXP_LEXER_RET_CONT);
 }
 
-int	exp_lexer_pop_quote(struct s_shell *shell, t_exp_lexer *lexer, char c)
+int	exp_lexer_pop_quote(struct s_shell *shell, char c, int mask)
 {
-	(void)shell;
 	(void)c;
-	exp_ss_pop(lexer);
+	(void)mask;
+	exp_ss_pop(&shell->exp_lexer);
 	return (EXP_LEXER_RET_CONT);
 }
