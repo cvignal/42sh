b0fa845ab82b91ab8d6b103d1d2df905839ff250
Guillaume Chainet <gchainet@student.42.fr>
2019-04-12 22:35:34 +0200

    Basic arithmetic execution for basic operations
    Former-commit-id: 4678929e08d6f9dac2ba4e19f97ff30b1eae9590

--- src/exec/arithmetic/eq.c
+++ src/exec/arithmetic/eq.c
@@ -6,7 +6,7 @@
 /*   By: gchainet <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/07 01:51:04 by gchainet          #+#    #+#             */
-/*   Updated: 2019/04/12 22:15:05 by gchainet         ###   ########.fr       */
+/*   Updated: 2019/04/12 22:22:51 by gchainet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,9 +21,9 @@ static int	exec_ari_eq_var(t_shell *shell, t_ast *ast)
 	int		ret;
 	char	*new_value;
 
+	ret = ast->right->exec(shell, ast->right);
 	if (ast->right->data)
 	{
-		ret = ast->right->exec(shell, ast->right);
 		if (!(new_value = ft_itoa(ret))
 				|| set_var(&shell->vars, ast->left->data, new_value, 0))
 			ft_dprintf(STDERR_FILENO, "%s: %s\n", EXEC_NAME, MEMORY_ERROR_MSG);
