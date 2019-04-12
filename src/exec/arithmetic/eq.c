ff29ef80d10aa8acef96e216581b61fdb8ae8bff
Guillaume Chainet <gchainet@student.42.fr>
2019-04-12 22:15:39 +0200

    Use MEMORY_ERR_MSG define in arithmetic equality execution
    Former-commit-id: 33586953c32f4980f9d90f9b67bbdaaf649b921d

--- src/exec/arithmetic/eq.c
+++ src/exec/arithmetic/eq.c
@@ -6,7 +6,7 @@
 /*   By: gchainet <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/07 01:51:04 by gchainet          #+#    #+#             */
-/*   Updated: 2019/04/10 07:19:33 by gchainet         ###   ########.fr       */
+/*   Updated: 2019/04/12 22:15:05 by gchainet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,11 +26,7 @@ static int	exec_ari_eq_var(t_shell *shell, t_ast *ast)
 		ret = ast->right->exec(shell, ast->right);
 		if (!(new_value = ft_itoa(ret))
 				|| set_var(&shell->vars, ast->left->data, new_value, 0))
-		{
-			ft_dprintf(STDERR_FILENO,
-					"%s: unable to allocate memory for variable assignement\n",
-					EXEC_NAME);
-		}
+			ft_dprintf(STDERR_FILENO, "%s: %s\n", EXEC_NAME, MEMORY_ERROR_MSG);
 		else
 		{
 			ast->ret = !ret;
