aa1599d9e54bdd927a7e4cfd473c3aa27b1f2268
Guillaume Chainet <gchainet@student.42.fr>
2019-04-13 08:51:55 +0200

    Remove check for empty ast nodes in rule_send_to_shunting_yard
    Former-commit-id: 4f6ffc85f4b36f65e1bbb44b6e6f6024a44d1186

--- src/parser/rules_shunting_yard.c
+++ src/parser/rules_shunting_yard.c
@@ -6,7 +6,7 @@
 /*   By: gchainet <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/07 06:48:20 by gchainet          #+#    #+#             */
-/*   Updated: 2019/04/10 05:54:07 by gchainet         ###   ########.fr       */
+/*   Updated: 2019/04/13 08:50:39 by gchainet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,9 +15,7 @@
 
 int	rule_send_to_shunting_yard(t_parser *parser, t_ast_token *list)
 {
-	if (list->type == TT_STATEMENT
-			&& (!list->data || !((t_ast *)list->data)->data))
-		return (1);
+	(void)list;
 	shunting_yard(parser);
 	return (0);
 }
