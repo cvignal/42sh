89dbce528b5f01f253f2a45344170af2417eb3b6
Charles VIGNAL <cvignal@student.42.fr>
2019-04-17 12:24:35 +0200

    Ctrl K implemented and basic autocompletion for tilde
    Former-commit-id: ff1ba872cf3a81b5d6e8e86016038f709529012c

---
+++ src/line_edition/tools_path.c
@@ -0,0 +1,58 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   tools_path.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cvignal <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/04/17 12:18:42 by cvignal           #+#    #+#             */
+/*   Updated: 2019/04/17 12:24:05 by cvignal          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+#include <term.h>
+#include <curses.h>
+
+#include "shell.h"
+#include "libft.h"
+#include "fill_line.h"
+
+static int	*expand_tilde(char *word, char *ret)
+{
+	ft_strdel(&ret);
+	if (!(ret = ft_strjoin("/Users/", getlogin())))
+		return (NULL);
+	if (!(ret = ft_strjoin_free(ret, word + 1, 1)))
+		return (NULL);
+	return (ret);
+}
+
+char		*find_path(char *word)
+{
+	char	pwd[BUFF_SIZE];
+	char	*ret;
+
+	getcwd(pwd, BUFF_SIZE);
+	ret = ft_strjoin(pwd, "/");
+	if (!word)
+		return (ret);
+	if (*word != '/' && (*word != '~' || !*(word + 1)))
+		ret = ft_strjoin_free(ret, word, 1);
+	else if (*word == '~' && *(word + 1))
+	{
+		if (!(ret = expand_tilde(word, ret)))
+			return (NULL);
+	}
+	else
+	{
+		ft_strdel(&ret);
+		if (!(ret = ft_strdup(word)))
+			return (NULL);
+	}
+	if (ft_strchr(word, '/'))
+		*(ft_strrchr(ret, '/') + 1) = 0;
+	else
+		*(ret + ft_strlen(pwd)) = 0;
+	return (ret);
+}
