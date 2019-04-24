df47b6abf908958b4ed6baf5cac0c0cef1963c37
Shamoka <gchainet@student.42.fr>
2019-04-24 18:31:26 +0200

    Fix syntax error detection

--- src/parser/rules_shunting_yard.c
+++ src/parser/rules_shunting_yard.c
@@ -6,7 +6,7 @@
 /*   By: gchainet <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/07 06:48:20 by gchainet          #+#    #+#             */
-/*   Updated: 2019/04/13 08:50:39 by gchainet         ###   ########.fr       */
+/*   Updated: 2019/04/24 18:24:40 by gchainet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,11 @@
 int	rule_send_to_shunting_yard(t_parser *parser, t_token *list)
 {
 	(void)list;
+	if (list->type == TT_STATEMENT)
+		parser->pss->status = PARSER_COMPLETE;
+	else if (list->type == TT_OP
+			&& ((t_ast *)list->data)->type != TT_END_UNARY)
+		parser->pss->status = PARSER_MORE_INPUT;
 	shunting_yard(parser);
 	return (0);
 }
