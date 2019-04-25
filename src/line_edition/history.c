e832e13f8a4b6aeefa1509f72a3253a1c932ef6e
Charles VIGNAL <cvignal@student.42.fr>
2019-04-25 11:24:04 +0200

    Current input is now saved in case of history navigation

--- src/line_edition/history.c
+++ src/line_edition/history.c
@@ -6,7 +6,7 @@
 /*   By: cvignal <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/21 10:29:40 by cvignal           #+#    #+#             */
-/*   Updated: 2019/04/09 11:54:07 by cvignal          ###   ########.fr       */
+/*   Updated: 2019/04/25 10:34:18 by cvignal          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,8 @@
 
 #include "fill_line.h"
 #include "libft.h"
+
+char	*g_save_line = NULL;
 
 void	clear_cmd_line(t_shell *shell)
 {
@@ -51,6 +53,11 @@ int		ft_hisdown(t_shell *shell)
 	free_line(&shell->line);
 	if (shell->his_pos < shell->history->length)
 		ft_addchar(shell, shell->history->data[shell->his_pos], 1);
+	else if (g_save_line)
+	{
+		ft_addchar(shell, g_save_line, 1);
+		ft_strdel(&g_save_line);
+	}
 	return (0);
 }
 
@@ -61,6 +68,13 @@ int		ft_hisup(t_shell *shell)
 	if (shell->his_pos > 0)
 		shell->his_pos--;
 	clear_cmd_line(shell);
+	if (shell->his_pos == shell->history->length - 1)
+	{
+		if (g_save_line)
+			ft_strdel(&g_save_line);
+		if (!(g_save_line = ft_strdup(shell->line.data)))
+			return (1);
+	}
 	free_line(&shell->line);
 	ft_addchar(shell, shell->history->data[shell->his_pos], 1);
 	return (0);
