1c6b9bf764581d25f1f9d87efff7cb56bc345244
Guillaume CHAINET <gchainet@student.42.fr>
2019-04-28 03:20:09 +0200

    Fix norme errors

--- src/expansion/lexer_methods_over.c
+++ src/expansion/lexer_methods_over.c
@@ -6,14 +6,14 @@
 /*   By: gchainet <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/27 23:43:07 by gchainet          #+#    #+#             */
-/*   Updated: 2019/04/27 23:45:17 by gchainet         ###   ########.fr       */
+/*   Updated: 2019/04/28 03:19:37 by gchainet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "shell.h"
 #include "arithmetic.h"
 
-int	exp_lexer_error(t_shell *shell, char c , int mask)
+int	exp_lexer_error(t_shell *shell, char c, int mask)
 {
 	(void)shell;
 	(void)c;
@@ -21,7 +21,7 @@ int	exp_lexer_error(t_shell *shell, char c , int mask)
 	return (EXP_LEXER_RET_ERROR);
 }
 
-int	exp_lexer_over(t_shell *shell, char c , int mask)
+int	exp_lexer_over(t_shell *shell, char c, int mask)
 {
 	(void)shell;
 	(void)c;
