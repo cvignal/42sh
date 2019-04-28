4defca11e29fdbb8e4df9d1f8292ec7df5aab72f
Guillaume CHAINET <gchainet@student.42.fr>
2019-04-28 04:51:17 +0200

    Fix && and || operators in arithmetic expressions
    Former-commit-id: 0484df5626e85c1fed21f77acf849bfcf2c69dae

--- src/exec/arithmetic/or.c
+++ src/exec/arithmetic/or.c
@@ -6,7 +6,7 @@
 /*   By: gchainet <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/28 03:02:32 by gchainet          #+#    #+#             */
-/*   Updated: 2019/04/28 03:07:23 by gchainet         ###   ########.fr       */
+/*   Updated: 2019/04/28 04:49:29 by gchainet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,10 +16,10 @@
 int	exec_ari_or(t_shell *shell, struct s_ast *ast)
 {
 	ast->left->exec(shell, ast->left);
-	if (ast->left->data && ast->left->ret)
+	if (ast->left->data && ft_atoi(ast->left->data))
 	{
 		ast->right->exec(shell, ast->right);
-		ast->ret = !!ast->right->ret;
+		ast->ret = !!ft_atoi(ast->right->data);
 	}
 	else
 		ast->ret = 1;
