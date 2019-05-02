07d509a2d2fa6d3e6c766383c7d319b493f4845f
Shamoka <gchainet@student.42.fr>
2019-05-03 00:39:30 +0200

    Fix || return value
    Former-commit-id: a1ae3a253c063ec8118925e98589e748495ef663

--- src/exec/or.c
+++ src/exec/or.c
@@ -6,7 +6,7 @@
 /*   By: gchainet <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/15 20:38:39 by gchainet          #+#    #+#             */
-/*   Updated: 2019/05/03 00:19:47 by gchainet         ###   ########.fr       */
+/*   Updated: 2019/05/03 00:38:55 by gchainet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,9 +25,9 @@ int		exec_or(t_shell *shell, t_ast *ast)
 	{
 		ast->right->exec(shell, ast->right);
 		wait_loop(shell, ast->right);
-		return ((ast->ret = 0));
+		return ((ast->ret = ast->right->ret));
 	}
-	return ((ast->ret = 1));
+	return ((ast->ret = 0));
 }
 
 void	free_or(t_ast *ast)
