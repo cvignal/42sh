f66c2cd0734e49463e35d10bb5036dba54de4f05
marin postma <postma.marin@protonmail.com>
2019-05-29 23:06:23 +0200

    expansion for certain params, not field splitting

--- src/expansion/special_params.c
+++ src/expansion/special_params.c
@@ -6,18 +6,20 @@
 /*   By: marin </var/spool/mail/marin>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/25 14:35:55 by marin             #+#    #+#             */
-/*   Updated: 2019/05/25 14:47:42 by marin            ###   ########.fr       */
+/*   Updated: 2019/05/27 23:33:51 by marin            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "shell.h"
+#include <unistd.h>
 
-static const char	g_special_params[5] = \
+static const char	g_special_params[6] = \
 {
 	SPECIAL_PARAM_AT,
 		SPECIAL_PARAM_STAR,
 		SPECIAL_PARAM_ZERO,
 		SPECIAL_PARAM_BANG,
+		SPECIAL_PARAM_DOLLAR,
 		SPECIAL_PARAM_HASH
 };
 
@@ -33,4 +35,14 @@ int			is_special_param(char name)
 		++i;
 	}
 	return (0);
+}
+
+char	*get_special_param_value(char *name, t_shell *shell)
+{
+	if (!ft_strcmp(name, "$"))
+		return (ft_itoa((int)getpid()));
+	if (!ft_strcmp(name, "#"))
+		return (ft_itoa(shell->arg_file ? shell->arg_file->argc : 0));
+	return "hello";
+
 }
---
+++ test.sh
@@ -0,0 +1,5 @@
+echo $#
+echo $@
+echo $*
+echo $!
+echo $$
