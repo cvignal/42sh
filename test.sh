5e8351ed218b9110140fe968f741a007d4e2b407
marin postma <postma.marin@protonmail.com>
2019-06-08 11:01:14 +0200

    tests updated
    Former-commit-id: 88de696768e3ca074fb95ba715e0077bb59f5a6a

--- test.sh
+++ test.sh
@@ -1,13 +1,18 @@
-echo $0
-echo $#
-echo $*
-echo $@
-echo $$
-echo $?
 
-echo ${0} 
-echo ${#}
-echo ${*}
-echo ${@}
-echo ${$}
-echo ${?}
+echo "invalid expansions"
+
+echo ${0 } 
+echo ${# }
+echo ${* }
+echo ${@ }
+echo ${$ }
+echo ${ ?}
+echo ${ 0} 
+echo ${ #}
+echo ${ *}
+echo ${ @}
+echo ${ $}
+echo ${ ?}
+echo ${HOME }
+echo ${ PWD}
+echo ${}
