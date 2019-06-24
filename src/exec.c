064ddb718354e31a5d79134a4717f687fe8dddcd
Guillaume Chainet <gchainet@student.42.fr>
2019-06-24 23:48:29 +0200

    Remove useless reset of builtin return value

--- src/exec.c
+++ src/exec.c
@@ -6,7 +6,7 @@
 /*   By: gchainet <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/12 09:03:28 by gchainet          #+#    #+#             */
-/*   Updated: 2019/06/24 19:37:24 by gchainet         ###   ########.fr       */
+/*   Updated: 2019/06/24 23:36:00 by gchainet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -100,9 +100,7 @@ int			exec_job(t_shell *shell, t_ast *node, t_job *job)
 	node->job = job2;
 	if (node->exec(shell, node))
 		return (-1);
-	if (shell->last_cmd)
-		set_ret(shell, node, node->ret);
-	else if (!job && job2->proc)
+	if (!job && job2->proc)
 		set_ret(shell, node, register_job(shell, job2));
 	else if (!job)
 		free(job2);
